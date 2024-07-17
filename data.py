604bad3da7d3ae96c8b22dda9ee4d11dabbe8e05
Alf <sh4mil.nasyrov@gmail.com>
2024-07-17 16:19:19 +0300

    prepare test environment

---
+++ data.py
@@ -0,0 +1,6 @@
+class TestData():
+    MAIN_URL = "https://stellarburgers.nomoreparties.site/"
+    LOGIN_URL = MAIN_URL + "login"
+    RECOVER_PASSWORD_URL = MAIN_URL + "forgot-password"
+    TESTER_EMAIL = "tester_123123@mail.ru"
+    
